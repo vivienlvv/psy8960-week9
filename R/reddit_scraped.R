1f8ac4c8f36f56378d60bf7e271fc3967c397b65
Vivien Lee <vivienlvv@gmail.com>
2023-03-23 15:37:01 -0500

    First pass html

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -2,17 +2,35 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(rvest)
+library(str)
 
 # Data Import and Cleaning
 rstats_html = read_html("https://old.reddit.com/r/rstats/")
 
 ## Note: Ads are scraped here
 post = html_elements(rstats_html, css = "a.title.may-blank") %>% html_text()
-upvotes = html_elements(rstats_html, css = "div.score.unvoted") %>% html_text()
-comments = html_elements(rstats_html, css = "li.first") %>% html_text() # Issue: Ads don't have comments
-
+upvotes = html_elements(rstats_html, css = "div.score.unvoted") %>% 
+  html_text() %>% 
+  as.numeric() # NA means hidden
+  
+comments = html_elements(rstats_html, xpath = "//div[@class = 'entry unvoted']//li[position() = 1]") %>% 
+  html_text() %>% 
+  str_replace_all(pattern = "^comment$", replacement = "0") %>% 
+  str_extract(pattern = "\\d+") %>% 
+  as.numeric() # NA means ads
+  
 rstats_tbl = tibble(post, upvotes, comments)
 
+# Visualization
+rstats_tbl %>% ggplot(aes(x = upvotes, y = comments)) + 
+  geom_point() + 
+  labs(x = "No. of Upvotes", 
+       y = "No. of Comments",
+       title = "Relationship between Upvotes and Comments")
 
-# a.title.may-blank.outbound | p.title
-# a.title.may-blank 
+# Analysis 
+cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments)
+
+# Publication (Section 6- confused by instructions, need to come back)
+# The correlation between upvotes and comments was r(X) = .XX, p = .XX
+
