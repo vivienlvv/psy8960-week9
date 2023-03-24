ba27ad041259d4cb2205a060e2d819eced95a0d4
Vivien Lee <vivienlvv@gmail.com>
2023-03-23 21:24:32 -0500

    Replacing css selector w/ xpaths to de-select ads

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -2,22 +2,28 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(rvest)
-library(str)
 
 # Data Import and Cleaning
 rstats_html = read_html("https://old.reddit.com/r/rstats/")
 
 ## Note: Ads are scraped here
-post = html_elements(rstats_html, css = "a.title.may-blank") %>% html_text()
-upvotes = html_elements(rstats_html, css = "div.score.unvoted") %>% 
+# post = html_elements(rstats_html, css = "a.title.may-blank.outbound") %>% html_text()
+# Generating xpaths for the three variables
+xpath_post = "//div[contains(@class, 'odd') or contains(@class, 'even')]//a[contains(@class, 'title may-blank')]"
+xpath_upvotes = "//div[contains(@class, 'odd') or contains(@class, 'even')]//div[@class = 'score unvoted']"
+xpath_comments = "//div[contains(@class, 'odd') or contains(@class, 'even')]//div[@class = 'entry unvoted']//li[position() = 1]"
+  
+post = html_elements(rstats_html, xpath = xpath_post) %>% html_text()
+
+upvotes = html_elements(rstats_html, xpath = xpath_upvotes) %>% 
   html_text() %>% 
   as.numeric() # NA means hidden
   
-comments = html_elements(rstats_html, xpath = "//div[@class = 'entry unvoted']//li[position() = 1]") %>% 
+comments = html_elements(rstats_html, xpath = xpath_comments) %>% 
   html_text() %>% 
   str_replace_all(pattern = "^comment$", replacement = "0") %>% 
   str_extract(pattern = "\\d+") %>% 
-  as.numeric() # NA means ads
+  as.numeric() 
   
 rstats_tbl = tibble(post, upvotes, comments)
 
