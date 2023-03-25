69d077d7cd32050b1512805eafcbc35ade642081
Vivien Lee <vivienlvv@gmail.com>
2023-03-25 14:48:02 -0500

    Chabges to data cleaning and cor display

--- R/reddit_api.R
+++ R/reddit_api.R
@@ -2,17 +2,16 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(jsonlite)
-# library(httr)
-# library(rvest)
 options(scipen = 999)
 
 # Data Import and Cleaning
 rstats_list = fromJSON("https://www.reddit.com/r/rstats/.json") 
 rstats_original_tbl = rstats_list$data$children$data
 
-rstats_tbl = tibble(post = rstats_original_tbl$title,
-                    upvotes = rstats_original_tbl$ups,
-                    comments = rstats_original_tbl$num_comments)
+rstats_tbl = rstats_original_tbl %>% 
+  transmute(post = title,
+            upvotes = ups, 
+            comments = num_comments) 
 
 # Visualization
 rstats_tbl %>% ggplot(aes(x = upvotes, y = comments)) + 
@@ -22,16 +21,17 @@ rstats_tbl %>% ggplot(aes(x = upvotes, y = comments)) +
        title = "Relationship between Upvotes and Comments")
 
 # Analysis 
-cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments)
+(cor_upvote_comment = cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments))
 
-# Publication (Section 6- confused by instructions, need to come back)
-# The correlation between upvotes and comments was r(23) = .75, p < .00
+# Publication 
+## The correlation betweeen upvotes and comments was r(23) = .38, p = .06
 
+## Removing leading zeroes in output and rounding 
+cor_output = tibble(df = cor_upvote_comment$parameter, 
+                    estimate = round(cor_upvote_comment$estimate, digits = 2),
+                    pval = round(cor_upvote_comment$p.value, digits = 2)) %>% 
+  mutate(across(-df, str_replace_all, pattern = "^0", replacement = ""))
 
-
-
-# Useless content to delete later
-# temp = read_html("https://www.reddit.com/r/rstats/")
-# tbl = tibble(post = html_text(html_elements(temp, "div.STit0dLageRsa2yR4te_b")),
-#              upvotes = html_text(html_elements(temp, "div._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo")),
-#              comments = html_text(html_elements(temp, "span.FHCV02u6Cp2zYL0fhQPsO")))
+## Constructing the outputted line of text dynamically
+paste0("The correlation betweeen upvotes and comments was r(",
+       cor_output$df, ") = ", cor_output$estimate, ", p = ", cor_output$pval)
