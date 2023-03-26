04f4fe12332765583bd9a370c2bdc3d2e6dae6a6
Vivien Lee <vivienlvv@gmail.com>
2023-03-26 17:03:07 -0500

    Added comments & updated pub

--- R/reddit_scraped.R
+++ R/reddit_scraped.R
@@ -2,6 +2,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
 library(rvest)
+options(scipen = 999)
 
 # Data Import and Cleaning
 rstats_html = read_html("https://old.reddit.com/r/rstats/")
@@ -11,18 +12,19 @@ rstats_html = read_html("https://old.reddit.com/r/rstats/")
 xpath_post = "//div[contains(@class, 'odd') or contains(@class, 'even')]//a[contains(@class, 'title may-blank')]"
 xpath_upvotes = "//div[contains(@class, 'odd') or contains(@class, 'even')]//div[@class = 'score unvoted']"
 xpath_comments = "//div[contains(@class, 'odd') or contains(@class, 'even')]//div[@class = 'entry unvoted']//li[position() = 1]"
-  
+
+## Creating vectors for constructing tibble later
 post = html_elements(rstats_html, xpath = xpath_post) %>% html_text()
 upvotes = html_elements(rstats_html, xpath = xpath_upvotes) %>% 
   html_text() %>% 
-  as.numeric() # NA means hidden
+  as.numeric() # NA means hidden posts, which will be dropped by ggplot() and cor.test() automatically
 comments = html_elements(rstats_html, xpath = xpath_comments) %>% 
   html_text() %>% 
   str_replace_all(pattern = "^comment$", replacement = "0") %>% 
   str_extract(pattern = "\\d+") %>% 
   as.numeric() 
 
-## Creating required variable  
+## Creating required variable rstats_tbl 
 rstats_tbl = tibble(post, upvotes, comments)
 
 # Visualization
@@ -33,22 +35,24 @@ rstats_tbl %>% ggplot(aes(x = upvotes, y = comments)) +
        title = "Relationship between Upvotes and Comments")
 
 # Analysis 
-(cor_upvote_comment2 = cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments))
+cor_upvote_comment2 = cor.test(x = rstats_tbl$upvotes, y = rstats_tbl$comments)
+cor_upvote_comment2$estimate
+cor_upvote_comment2$p.value
 
 # Publication 
-# The correlation betweeen upvotes and comments was r(22) = .41, p = .04. This test was statistically significant.
+## The correlation betweeen upvotes and comments was r(21) = .15, p = .49. This test was not statistically significant.
 
-## Removing leading zeroes in output and rounding 
+## Cleaning correlation output (i.e., rounding & removing leading zeroes) to output required text
 cor_output2 = tibble(df = cor_upvote_comment2$parameter, 
                     estimate = round(cor_upvote_comment2$estimate, digits = 2),
                     pval = round(cor_upvote_comment2$p.value, digits = 2)) %>% 
-  mutate(across(-df, ~ str_replace_all(.x, pattern = "^0", replacement = "")))
-sig_flag2 = ifelse(cor_output2$pval <= .05, " ", " not") 
+  mutate(sig_flag = ifelse(pval <= .05, " ", " not "),
+         across(-df, ~ str_replace_all(.x, pattern = "^0", replacement = "")))
 
 ## Constructing the outputted line of text dynamically
 paste0("The correlation betweeen upvotes and comments was r(",
        cor_output2$df, ") = ", cor_output2$estimate, ", p = ", cor_output2$pval,
-       ". This test was", sig_flag2, "statistically significant.")
+       ". This test was", cor_output2$sig_flag, "statistically significant.")
 
 
 
